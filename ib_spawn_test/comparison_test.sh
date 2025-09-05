fe29c8d6d38b3153d51bf3ba7f3ad5e66acd9107
gmcavallazzi <gio.cava10@gmail.com>
2025-09-05 23:53:39 +0100

    new test1

--- ib_spawn_test/comparison_test.sh
+++ ib_spawn_test/comparison_test.sh
@@ -11,7 +11,18 @@
 module load compilers/gcc/11.2.0
 module load mpi/openmpi/4.1.1
 
-# Activate conda environment
+# Conda setup
+__conda_setup="$('/users/addh496/sharedscratch/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/users/addh496/sharedscratch/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "/users/addh496/sharedscratch/anaconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/users/addh496/sharedscratch/anaconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
 conda activate stw_pettingzoo
 export PYTHONPATH=$(python -c "import sys; print(':'.join(sys.path))")
 
@@ -33,10 +44,11 @@ mpirun \
   --mca btl tcp,self \
   --mca btl_tcp_if_include ib0 \
   --mca btl_base_warn_component_unused 0 \
+  --mca btl_base_verbose 0 \
   --hostfile hostfile \
   --bind-to core \
   -n 1 \
-  python python_spawn_test.py
+  python python_spawn_test.py 2>&1 | grep -v -E "(mca:|btl:|select:|bml:)"
 
 echo ""
 echo "--- TEST 2: PERSISTENT METHOD (Python + Python workers) ---"
@@ -44,10 +56,11 @@ mpirun \
   --mca btl tcp,self \
   --mca btl_tcp_if_include ib0 \
   --mca btl_base_warn_component_unused 0 \
+  --mca btl_base_verbose 0 \
   --hostfile hostfile \
   --bind-to core \
   -n 9 \
-  python python_persistent_test.py
+  python python_persistent_test.py 2>&1 | grep -v -E "(mca:|btl:|select:|bml:)"
 
 # Clean up
 rm -f hostfile
